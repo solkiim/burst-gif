89fa59b961278a2085e59bf91d318f85b7003603
Sol Kim <solkim813@gmail.com>
2017-01-10 23:53:17 -0800

    burst gif creation

--- js/js.js
+++ js/js.js
@@ -1,58 +1,64 @@
-// Put event listeners into place
+// put event listeners into place
 window.addEventListener("DOMContentLoaded", function() {
-	// Grab elements, create settings, etc.
+	// canvas settings
 	var canvas = document.getElementById('canvas');
 	var context = canvas.getContext('2d');
+	
+	// media settings
 	var video = document.getElementById('video');
 	var mediaConfig = { video: true };
 	var errBack = function(e) {
 		console.log('An error has occurred!', e)
 	};
+	
+	// jsgif encoder settings
+	var encoder = new GIFEncoder();
+	encoder.setRepeat(0);	// loop forever
+	encoder.setDelay(500);	// go to next frame every 500 milliseconds
+	encoder.start();
 
-	// Put video listeners into place
+	// put video listeners into place
 	if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
 		navigator.mediaDevices.getUserMedia(mediaConfig).then(function(stream) {
 			video.src = window.URL.createObjectURL(stream);
 			video.play();
 		});
 	}
-
-	/* Legacy code below! */
-	else if(navigator.getUserMedia) { // Standard
+	// legacy code
+	else if(navigator.getUserMedia) { 			// standard
 		navigator.getUserMedia(mediaConfig, function(stream) {
 			video.src = stream;
 			video.play();
 		}, errBack);
-	} else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
+	} else if(navigator.webkitGetUserMedia) {	// webKit-prefixed
 		navigator.webkitGetUserMedia(mediaConfig, function(stream){
 			video.src = window.webkitURL.createObjectURL(stream);
 			video.play();
 		}, errBack);
-	} else if(navigator.mozGetUserMedia) { // Mozilla-prefixed
+	} else if(navigator.mozGetUserMedia) {		// mozilla-prefixed
 		navigator.mozGetUserMedia(mediaConfig, function(stream){
 			video.src = window.URL.createObjectURL(stream);
 			video.play();
 		}, errBack);
 	}
 
-	// Trigger photo take
+	// snap burst trigger
 	document.getElementById('snap').addEventListener('click', function() {
-		(function myLoop (i) {
-		   setTimeout(function() {
-			   console.log(i);
-			   context.drawImage(video, 0, 0, 100, 75);
-			  if (--i) myLoop(i);      //  decrement i and call myLoop again if i > 0
-		   }, 500)
-		})(10);
+		encoder.start();
 		
-		//convertCanvasToImage(canvas);
+		(function myLoop (i) {
+			setTimeout(function() {
+				context.drawImage(video, 0, 0, 1024, 768);
+				encoder.addFrame(context);	// add to encoder
+				if (--i) {
+					myLoop(i);      		// decrement i and call myLoop again if i > 0
+				} else {
+					encoder.finish();		// finish gif
+					var binary_gif = encoder.stream().getData() //notice this is different from the as3gif package!
+					var data_url = 'data:image/gif;base64,'+encode64(binary_gif);
+					window.location = data_url;
+				}
+			}, 500)
+		})(10);
 	});
 }, false);
-
-// Converts canvas to an image
-function convertCanvasToImage(canvas) {
-	var image = new Image();
-	image.src = canvas.toDataURL("image/png");
-	//console.log(image);
-	return image;
-}
